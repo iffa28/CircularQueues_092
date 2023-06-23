93e48830fff9e166757c9c24ef8def6580c1d68b
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-06-23 15:30:41 +0700

    iterasi dari awal array hingga rear

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -82,6 +82,17 @@ public:
 		}
 		else {
 			//jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir REAR
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			//iterasi dari awal array hingga rear
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+			}
 		}
 	}
 };
